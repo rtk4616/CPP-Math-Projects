4c27ed71e77ce0047dcd2f556fcbdc44cb13c87a
fusion809 <brentonhorne77@gmail.com>
2016-07-13 20:59:38 +1000

    Updating plotting function

--- example.cpp
+++ example.cpp
@@ -60,5 +60,5 @@ int main()
     myfile << t[N];
 		myfile << " " << x[N];
 		myfile.close();
-		system("gnuplot -p -e \"plot 'example.txt'\"");
+		system("gnuplot -p -e \"set style line 1 lc rgb '#0060ad' lt 1 lw 1 pt 1 ps 1.0; plot 'example.txt' with linespoints ls 1\"");
 }
