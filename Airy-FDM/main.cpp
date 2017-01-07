c619c2d7b3c1597737034abcf0e62980e4a48b97
Brenton Horne <brentonhorne77@gmail.com>
2017-01-07 10:41:41 +1000

    Updating

---
+++ Airy-FDM/main.cpp
@@ -0,0 +1,6 @@
+// Brenton Horne Airy Finite Difference Method solver
+
+double N = 1000.0;
+double a = 0.0;
+double b = 100.0;
+double h = (b-a) / N;
--- Simple-Pendulum/simple-pendulum.cpp
+++ Simple-Pendulum/simple-pendulum.cpp
@@ -11,14 +11,16 @@ using namespace std;
 
 double N     = 100000.0;                 // Number of domain elements we're integrating over
 double t0    = 0.0;                      // Starting time
-double t1    = 10;
-double x0    = 0.0;
-double dx0   = 0.0;
+double t1    = 10;                       // End time
+double x0    = 0.0;                      // Initial angle from the x axis
+double dx0   = 0.0;                      // Initial rate of change in angle from x axis
+double g     = 9.8;                      // acceleration due to gravity in m/s^2
+double l     = 1.0;                      // Length of pendulum bob
 double h     = (t1 - t0) / double(N);    // step size
 
-double dx2(double t, double x, double dx)
+double dx2(double g, double l, double t, double x, double dx)
 {
-    return -9.8*cos(x);
+    return - ( g / l ) * cos(x);
 }
 
 std::pair<double, double> RK4(double t, double x, double dx, double h)
@@ -26,16 +28,16 @@ std::pair<double, double> RK4(double t, double x, double dx, double h)
     // k values are diffs in dy/dx.
     // l values are diffs in x.
     double k1, k2, k3, k4, l1, l2, l3, l4, diff1, diff2;
-    k1 = h*dx2(t,x,dx);
-    l1 = h*dx;
-    k2 = h*dx2(t+h/2,x+l1/2,dx+k1/2);
-    l2 = h*(dx+k1/2);
-    k3 = h*dx2(t+h/2,x+l2/2,dx+k2/2);
-    l3 = h*(dx+k2/2);
-    k4 = h*dx2(t+h,x+l3,dx+k3);
-    l4 = h*(dx+k3);
-    diff1 = (l1+2*l2+2*l3+l4)/float(6);  // diff in x.
-    diff2 = (k1+2*k2+2*k3+k4)/float(6);  // diff in y.
+    k1    = h * dx2(g, l, t,       x,        dx);
+    l1    = h * dx;
+    k2    = h * dx2(g, l, t + h/2, x + l1/2, dx + k1/2);
+    l2    = h * (dx + k1/2);
+    k3    = h * dx2(g, l, t + h/2, x + l2/2, dx + k2/2);
+    l3    = h * (dx + k2/2);
+    k4    = h * dx2(g, l, t + h,   x + l3,   dx + k3);
+    l4    = h * (dx + k3);
+    diff1 = (l1 + 2 * l2 + 2 * l3 + l4)/float(6);  // diff in x.
+    diff2 = (k1 + 2 * k2 + 2 * k3 + k4)/float(6);  // diff in y.
     return {diff1, diff2};
 }
 
@@ -60,21 +62,21 @@ int main()
     for(int i = 1; i<=N; i++)
     {
         std::pair<double, double> diff = RK4(t[i-1],x[i-1],dx[i-1],h);
-        t.push_back(  t[i-1] + h);
-        x.push_back(  x[i-1] + diff.first  );
-        dx.push_back(dx[i-1] + diff.second );
-        myfile << t[i-1];
-        myfile << " " << x[i-1] << "\n";
-        myfiledx << t[i-1];
-        myfiledx << " " << dx[i-1] << "\n";
+        t.push_back(   t[i-1] + h);
+        x.push_back(   x[i-1] + diff.first  );
+        dx.push_back( dx[i-1] + diff.second );
+        myfile      << t[i-1];
+        myfile      << " "     << x[i-1]   << "\n";
+        myfiledx    << t[i-1];
+        myfiledx    << " "     << dx[i-1]  << "\n";
         myfilephase << x[i-1];
-        myfilephase << " " << dx[i-1] << "\n";
+        myfilephase << " "     << dx[i-1]  << "\n";
         usleep(1000);
     }
-    myfile << t[N];
-    myfile << " " << x[N];
+    myfile   << t[N];
+    myfile   << " "   << x[N];
     myfiledx << t[N];
-    myfiledx << " " << dx[N];
+    myfiledx << " "   << dx[N];
     myfile.close();
     myfiledx.close();
     system("gnuplot -p simple-pendulum.gp");
