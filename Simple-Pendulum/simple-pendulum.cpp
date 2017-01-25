95ffe42e5e032dd500efe2c8ba7cca2bacacb754
Brenton Horne <brentonhorne77@gmail.com>
2017-01-25 15:48:23 +1000

    Spacing out simple-pendulum.cpp

--- Simple-Pendulum/simple-pendulum.cpp
+++ Simple-Pendulum/simple-pendulum.cpp
@@ -34,14 +34,19 @@ std::pair<double, double> RK4(double t, double x, double dx, double h)
     // ln values are diffs in x.
     double k1    = h * dx2(g, l, t,       x,        dx);
     double l1    = h * dx;
+
     double k2    = h * dx2(g, l, t + h/2, x + l1/2, dx + k1/2);
     double l2    = h * (dx + k1/2);
+
     double k3    = h * dx2(g, l, t + h/2, x + l2/2, dx + k2/2);
     double l3    = h * (dx + k2/2);
+
     double k4    = h * dx2(g, l, t + h,   x + l3,   dx + k3);
     double l4    = h * (dx + k3);
+
     double diff1 = (l1 + 2 * l2 + 2 * l3 + l4)/float(6);  // diff in x.
     double diff2 = (k1 + 2 * k2 + 2 * k3 + k4)/float(6);  // diff in y.
+
     return {diff1, diff2};
 }
 
@@ -49,48 +54,65 @@ int main()
 {
     std::vector<double> t;
     t.push_back(t0);
+
     std::vector<double> x;
     x.push_back(x0);
+
     std::vector<double> dx;
     dx.push_back(dx0);
+
     ofstream myfile;
     myfile.open("simple-pendulum.txt");
+
     ofstream myfiledx;
     myfiledx.open("simple-pendulum-dx.txt");
+
     ofstream myfilephase;
     myfilephase.open("simple-pendulum-phase.txt");
+
     double minx = x[0];
 
     for(int i = 1; i<=N; i++)
     {
         std::pair<double, double> diff = RK4(t[i-1],x[i-1],dx[i-1],h);
+
         t.push_back(   t[i-1] + h);
         x.push_back(   x[i-1] + diff.first  );
         dx.push_back( dx[i-1] + diff.second );
+        
         myfile      << t[i-1];
         myfile      << " "     << x[i-1]   << "\n";
+
         myfiledx    << t[i-1];
         myfiledx    << " "     << dx[i-1]  << "\n";
+
         myfilephase << x[i-1];
         myfilephase << " "     << dx[i-1]  << "\n";
+
         if(x[i]<minx)
         minx=x[i];
+
         usleep(1000);
     }
 
     myfile   << t[N];
     myfile   << " "   << x[N];
+
     myfiledx << t[N];
     myfiledx << " "   << dx[N];
+
     myfile.close();
     myfiledx.close();
+
     system("gnuplot -p simple-pendulum.gp");
     system("rsvg-convert -w 2000 -o simple-pendulum-standard.png simple-pendulum-standard.svg");
     system("rsvg-convert -w 2000 -o simple-pendulum-derivative.png simple-pendulum-derivative.svg");
     system("rsvg-convert -w 2000 -o simple-pendulum-phase.png simple-pendulum-phase.svg");
+
     std::cout << "Error:    " << std::scientific;
     std::cout.precision(15);
     std::cout << minx + M_PI;
     std::cout << ".\nx[N] is: " << x[N] << ".";
+
     return 0;
 }
