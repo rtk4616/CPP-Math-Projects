b26c54fa87a1eeaddb089b39e33195a17915da67
Brenton Horne <brentonhorne77@gmail.com>
2017-03-14 19:32:16 +1000

    Updating

--- Lorenz-equations-mysolver/lorenz.cpp
+++ Lorenz-equations-mysolver/lorenz.cpp
@@ -1,5 +1,7 @@
 // Lorenz equation solver
 // Copyright (C) 2017 Brenton Horne
+// Requires C++11 support, as tuple is a type found only in C++11 or later
+// Source (of tuples only existing in C++11 and later): http://en.cppreference.com/w/cpp/utility/tuple
 #include <iostream>
 #include <tuple>
 #include <vector>
@@ -13,16 +15,17 @@
 // Standard template library
 using namespace std;
 
-double N     = 1000000.0;                  // Number of domain elements we're integrating over
+// Number of domain elements we're integrating over
+double N     = 1000000.0;
 
 // Integration interval
 double t0    = 0.0;                       // Starting time
 double t1    = 100;                       // End time
 
 // Initial conditions
-double X0    = 1.0;
-double Y0    = 1.0;
-double Z0    = 1.0;
+double X0    = 1.0;                       // x(t0)
+double Y0    = 1.0;                       // y(t0)
+double Z0    = 1.0;                       // z(t0)
 
 // Parameters, a copy of this equation is in this directory
 double rho   = 28;
@@ -35,10 +38,14 @@ double h     = (t1 - t0) / double(N);
 // dX is a function that returns a 3-tuple; first entry is dx/dt, second is dy/dt and third is dz/dt.
 std::tuple<double, double, double> dX(double h, double sigma, double rho, double beta, double t, double x, double y, double z)
 {
-    return {h * sigma * (y - x), h * x * (rho - z) - h * y, h * x * y - h * beta * z};
+    // Used to use:
+	// return {h * sigma * (y - x), h * x * (rho - z) - h * y, h * x * y - h * beta * z}
+	// But that is inconsistent with GCC < 6 and Clang. 
+	// Eugene Porotov (https://www.facebook.com/eugene.porotov?fref=ufi) came up with the idea to use make_tuple instead
+    return std::make_tuple(h * sigma * (y - x), h * x * (rho - z) - h * y, h * x * y - h * beta * z);
 }
 
-// RK4 is a function that returns a 3-tuple; first entry is the diff in x, second is diff in y and third is diff in z. 
+// RK4 is a function that returns a 3-tuple; first entry is the diff in x, second is diff in y and third is diff in z.
 std::tuple<double, double, double> RK4(double t, double x, double y, double z, double h)
 {
     // kn values are diffs in x.
@@ -73,7 +80,7 @@ std::tuple<double, double, double> RK4(double t, double x, double y, double z, d
     double diffy = (l1 + 2 * l2 + 2 * l3 + l4)/float(6);  // diff in y.
     double diffz = (m1 + 2 * m2 + 2 * m3 + m4)/float(6);  // diff in z.
 
-    return {diffx, diffy, diffz};
+    return std::make_tuple(diffx, diffy, diffz);
 }
 
 int main()
@@ -94,124 +101,125 @@ int main()
     std::vector<double> z;
     z.push_back(Z0); // add Z0 to vector
 
-    // open myfilex ofstream
-    ofstream myfilex;
-    myfilex.open("lorenz-x.txt");
+    // open filetx ofstream
+    ofstream filetx;
+    filetx.open("lorenz-x.txt");
 
-    // open myfiley ofstream
-    ofstream myfiley;
-    myfiley.open("lorenz-y.txt");
+    // open filety ofstream
+    ofstream filety;
+    filety.open("lorenz-y.txt");
 
-    // open myfilez ofstream
-    ofstream myfilez;
-    myfilez.open("lorenz-z.txt");
+    // open filetz ofstream
+    ofstream filetz;
+    filetz.open("lorenz-z.txt");
 
-    // open myfilexy ofstream
-    ofstream myfilexy;
-    myfilexy.open("lorenz-xy.txt");
+    // open filexy ofstream
+    ofstream filexy;
+    filexy.open("lorenz-xy.txt");
 
-    // open myfilexz ofstream
-    ofstream myfilexz;
-    myfilexz.open("lorenz-xz.txt");
+    // open filexz ofstream
+    ofstream filexz;
+    filexz.open("lorenz-xz.txt");
 
-    // open myfileyz ofstream
-    ofstream myfileyz;
-    myfileyz.open("lorenz-yz.txt");
+    // open fileyz ofstream
+    ofstream fileyz;
+    fileyz.open("lorenz-yz.txt");
 
-    // open myfilephase ofstream
-    ofstream myfilephase;
-    myfilephase.open("lorenz-phase.txt");
+    // open filexyz ofstream
+    ofstream filexyz;
+    filexyz.open("lorenz-phase.txt");
 
     // Loop over elements in t
     for(int i = 1; i<=N; i++)
     {
+
         std::tuple<double, double, double> diff = RK4(t[i-1], x[i-1], y[i-1], z[i-1], h);
 
-	// add an ith element to t
+        // add an ith element to t
         t.push_back( t[i-1] + h                 );
         // add an ith element to x
         x.push_back( x[i-1] + std::get<0>(diff) );
         // add an ith element to y
         y.push_back( y[i-1] + std::get<1>(diff) );
-	// add an ith element to z
-	z.push_back( z[i-1] + std::get<2>(diff) );
+        // add an ith element to z
+        z.push_back( z[i-1] + std::get<2>(diff) );
 
-	// myfilex (i-1)th entry
-        myfilex     << t[i-1];
-        myfilex     << " "     << x[i-1]   << "\n";
-	myfilex.precision(15);
+        // filetx (i-1)th entry
+        filetx     << t[i-1];
+        filetx     << " "     << x[i-1]   << "\n";
+        filetx.precision(15);
 
-	// myfiley (i-1)th entry
-        myfiley     << t[i-1];
-        myfiley     << " "     << y[i-1]  << "\n";
-	myfiley.precision(15);
+        // filety (i-1)th entry
+        filety     << t[i-1];
+        filety     << " "     << y[i-1]  << "\n";
+        filety.precision(15);
 
-	// myfilez (i-1)th entry
-        myfilez     << t[i-1];
-        myfilez     << " "     << z[i-1]  << "\n";
-	myfilez.precision(15);
+        // filetz (i-1)th entry
+        filetz     << t[i-1];
+        filetz     << " "     << z[i-1]  << "\n";
+        filetz.precision(15);
 
-	// myfilexy (i-1)th entry
-	myfilexy    << x[i-1];
-        myfilexy    << " "     << y[i-1]  << "\n";
-	myfilexy.precision(15);
+        // filexy (i-1)th entry
+        filexy    << x[i-1];
+        filexy    << " "     << y[i-1]  << "\n";
+        filexy.precision(15);
 
-	// myfilexz (i-1)th entry
-	myfilexz    << x[i-1];
-        myfilexz    << " "     << z[i-1]  << "\n";
-	myfilexz.precision(15);
+        // filexz (i-1)th entry
+        filexz    << x[i-1];
+        filexz    << " "     << z[i-1]  << "\n";
+        filexz.precision(15);
 
-	// myfileyz (i-1)th entry
-	myfileyz    << y[i-1];
-        myfileyz    << " "     << z[i-1]  << "\n";
-	myfileyz.precision(15);
+        // fileyz (i-1)th entry
+        fileyz    << y[i-1];
+        fileyz    << " "     << z[i-1]  << "\n";
+        fileyz.precision(15);
 
-	// myfilephase (i-1)th entry
-	myfilephase << x[i-1];
-	myfilephase << " "     << y[i-1];
-	myfilephase << " "     << z[i-1]  << "\n";
-	myfilephase.precision(15);
+        // filexyz (i-1)th entry
+        filexyz << x[i-1];
+        filexyz << " "     << y[i-1];
+        filexyz << " "     << z[i-1]  << "\n";
+        filexyz.precision(15);
 
         usleep(1000);
     }
 
-    // myfilex Nth entry
-    myfilex  << t[N];
-    myfilex  << " "   << x[N];
+    // filetx Nth entry
+    filetx  << t[N];
+    filetx  << " "   << x[N];
 
-    // myfiley Nth entry
-    myfiley  << t[N];
-    myfiley  << " "   << y[N];
+    // filety Nth entry
+    filety  << t[N];
+    filety  << " "   << y[N];
 
-    // myfilez Nth entry
-    myfilez  << t[N];
-    myfilez  << " "   << z[N];
+    // filetz Nth entry
+    filetz  << t[N];
+    filetz  << " "   << z[N];
 
-    // myfilexy Nth entry
-    myfilexy << x[N];
-    myfilexy << " "   << y[N];
+    // filexy Nth entry
+    filexy << x[N];
+    filexy << " "   << y[N];
 
-    // myfilexz Nth entry
-    myfilexz << x[N];
-    myfilexz << " "   << z[N];
+    // filexz Nth entry
+    filexz << x[N];
+    filexz << " "   << z[N];
 
-    // myfileyz Nth entry
-    myfileyz << y[N];
-    myfileyz << " "   << z[N];
+    // fileyz Nth entry
+    fileyz << y[N];
+    fileyz << " "   << z[N];
 
-    // myfilephase Nth entry
-    myfilephase << x[N]; 
-    myfilephase << " " << y[N];
-    myfilephase << " " << z[N];
+    // filexyz Nth entry
+    filexyz << x[N];
+    filexyz << " " << y[N];
+    filexyz << " " << z[N];
 
     // close myfile streams
-    myfilex.close();
-    myfiley.close();
-    myfilez.close();
-    myfilexy.close();
-    myfilexz.close();
-    myfileyz.close();
-    myfilephase.close();
+    filetx.close();
+    filety.close();
+    filetz.close();
+    filexy.close();
+    filexz.close();
+    fileyz.close();
+    filexyz.close();
 
     // Generate svg plots
     system("gnuplot -p lorenz.gp");
